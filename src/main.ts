e392dd29c51aab0e83dc01f7732835f1c7346b18
DIMA <Pecherskiy@vector-best.ru>
2024-03-27 17:43:55 +0700

    hello deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-  sdfgsfdgsfdgssfdg
+  hello deploy
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
